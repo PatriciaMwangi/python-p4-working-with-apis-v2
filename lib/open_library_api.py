fb3985ff73fbe22f6163b442aa9e1b7654391d62
Patricia Mwangi <patricia.mwangi@student.moringaschool.com>
2024-07-04 16:07:25 +0300

    apis
    git commit -m apis

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -45,13 +45,18 @@ class Search:
         return response_formatted
 
 
-# results = Search().get_search_results()
-# print(results)
+results = Search().get_search_results()
+print(results)
 
-# results_json = Search().get_search_results_json()
-# print(json.dumps(results_json, indent=1))
+results_json = Search().get_search_results_json()
+print(json.dumps(results_json, indent=1))
 
 search_term = input("Enter a book title: ")
-result = Search().get_user_search_results(search_term)
+users=Search().get_user_search_results(search_term)
+print(users)
+
+
+#search_term = input("Enter a book title: ")
+#result = Search().get_user_search_results(search_term)
 print("Search Result:\n")
-print(result)
+#print(result)
